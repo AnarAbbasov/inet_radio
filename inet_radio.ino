7608f3e67f608e1774a357a633eab55d2c416739
Anar <anar@rocky.anar.com>
2025-01-25 21:56:46 -0800

    https

--- inet_radio.ino
+++ inet_radio.ino
@@ -1,126 +1,87 @@
-#include <VS1053Driver.h>
+#include <VS1053.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
 
 
-
-
-// include VS1053 library
-#include <ESP8266WiFi.h> 			
-// include ESP8266WiFi library
 int CS = D2;
-int DCS = D3; 				
-// define VS1053 decoder pins
+int DCS = D3;
 int DREQ = D1;
-VS1053 decoder(CS, DCS, DREQ); 		
-// associate decoder with VS1053
-int statPin = D8; 				
-// define switch pins for
-int volPin = D4; 				
-// station and volume
-WiFiClient client; 				
-// associate client and library
-char ssid[] = "Netzwerk"; 			
-// change xxxx to Wi-Fi ssid
-char password[] = "TedMiller@2023"; 			
-// change xxxx to Wi-Fi password
-const int maxStat = 4; 			
-// number of radio stations
-String stationName[] = {"1940 UK", "Bayern3", "ClassicFM", "BBC4"};
-char * host[maxStat] = {"h14.myradiostream.com/30454/listen.mp3", // station host
-"streams.br.de",
-"media-ice.musicradio.com",
-"bbcmedia.ic.llnwd.net"};
-char * path[maxStat] = {"/stream/1/",
-"/bayern3_2.m3u",
-"/ClassicFMMP3",
-"/stream/bbcmedia_radio4fm_mf_q"};
-int port[] = {8100,80,80,80}; 		
-// default station port is 80
-unsigned char mp3buff[32]; 			
-// VS1053 loads data in 32 bytes
+VS1053 decoder(CS, DCS, DREQ);
+int statPin = D8;
+int volPin = D4;
+WiFiClientSecure client;
+char ssid[] = "Netzwerk";
+char password[] = "TedMiller@2023";
+const int maxStat = 4;
+String stationName[] = {"Araz", "yurdfm", "ClassicFM", "mugam"};
+char * host[maxStat] = {"relay1.slayradio.org",
+                        "icecast.livetv.az",
+                        "media-ice.musicradio.com",
+                        "icecast.livetv.az"};
+char * path[maxStat] = {"/antennfm",
+                        "/yurdfm",
+                        "/ClassicFMMP3",
+                        "/mediamugam"};
+int port[] = {443, 443, 80, 443};
+unsigned char mp3buff[120];
 int station = 0;
-int volume = 0; 				
-// volume level 0-100
-volatile int newStation = 2; 			
-// station number at start up
-volatile int newVolume = 80; 			
-// volume at start up
-
-
+int volume = 0;
+volatile int newStation = 0;
+volatile int newVolume = 100;
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200); 		
-// Serial Monitor baud rate
-SPI.begin(); 			
-// initialise SPI bus
-decoder.begin(); 			
-// initialise VS1053 decoder
-decoder.switchToMp3Mode();
-// MP3 format mode
-decoder.setVolume(volume);
-// set decoder volume
-WiFi.begin(ssid, password);
-// initialise Wi-Fi
-while (WiFi.status() != WL_CONNECTED) delay(500);
-Serial.println("WiFi connected"); // wait for Wi-Fi connection
-pinMode(volPin, INPUT_PULLUP);
-// switch pin uses internal
-					// pull-up resistor
-attachInterrupt(digitalPinToInterrupt(statPin), chan, RISING);
-attachInterrupt(digitalPinToInterrupt(volPin), vol, FALLING);
+    Serial.begin(9600);
+    SPI.begin();
+    decoder.begin();
+    decoder.switchToMp3Mode();
+    //decoder.streamModeOn();
+    decoder.setVolume(volume);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) delay(500);
+    Serial.println("WiFi connected");
+    pinMode(volPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(statPin), chan, RISING);
+    attachInterrupt(digitalPinToInterrupt(volPin), vol, FALLING);
+    client.setInsecure();  // Optional: Accept any server certificate (for testing purposes only)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-if(station != newStation)
-// new station selected
-{
-station = newStation;
-// display updated station name
-Serial.print("connecting to CH"); Serial.print(station);
-Serial.print(" ");Serial.println(stationName[station]);
-if(client.connect(host[station], port[station]));
-{ 				
-// connect to radio station URL
-client.println(String("GET ")+ path[station] + " HTTP/1.1");
-Serial.println(String("GET ")+ path[station]);
-client.println(String("Host: ") + host[station]);
-client.println("Connection: close");
-client.println(); 	
-Serial.print("connected"); 
-// new line is required
-}
-}
-if(volume != newVolume) 		
-// change volume selected
-{
-volume = newVolume; 		
-// display updated volume
-Serial.print("volume ");Serial.println(volume);
-decoder.setVolume(volume);
-// set decoder volume
-}
-if(client.available() > 0)
-// when audio data available
-{				
-  Serial.print("playing ");
-// decode data 32 bytes at a time
-uint8_t bytesread = client.read(mp3buff, 32);
-decoder.playChunk(mp3buff, bytesread);
-}
+    if (station != newStation) {
+        station = newStation;
+        Serial.print("connecting to CH"); Serial.print(station);
+        Serial.print(" "); Serial.println(stationName[station]);
+        if (client.connect(host[station], port[station])) {
+            Serial.println("connected");
+            client.println(String("GET ") + path[station] + " HTTP/1.1");
+            Serial.println(String("GET ") + path[station]);
+            client.println(String("Host: ") + host[station]);
+            Serial.println(String("Host ") + host[station]);
+            client.println("Connection: close");
+            client.println();
+        }
+    }
+    if (volume != newVolume) {
+        volume = newVolume;
+        Serial.print("volume "); Serial.println(volume);
+        decoder.setVolume(volume);
+    }
+    if (client.available() > 0) {
+        
+       uint8_t bytesread= client.read(mp3buff, 120);
+        Serial.println(bytesread);
+        
+        decoder.playChunk(mp3buff, bytesread);
+        
+        
+    }
 }
 
+IRAM_ATTR void chan() {
+    newStation++;
+    if (newStation > maxStat - 1) newStation = 0;
+}
 
-IRAM_ATTR void chan() 		
-// ISR to increment station number
-{
-newStation++;
-if(newStation > maxStat-1) newStation = 0;
-}					
-// stations numbered 0, 1, 2...
-IRAM_ATTR void vol() 			
-// ISR to increase volume
-{
-newVolume = newVolume + 5;
-if(newVolume > 101) newVolume = 50;
-}	
+IRAM_ATTR void vol() {
+    newVolume = newVolume + 5;
+    if (newVolume > 101) newVolume = 50;
+}
